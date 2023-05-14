dde745a799114f559c7e4d15d34f82395f91118e
Nicolas Mitestainer <nicolas.mitestainer08@gmail.com>
2023-05-14 16:09:12 -0300

    animação-ao-scroll

--- script.js
+++ script.js
@@ -37,3 +37,26 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.5;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimacaoScroll()
